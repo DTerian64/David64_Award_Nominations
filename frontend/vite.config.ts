25bac18eaa4bb562b8e1e0b410acb4464ab05c47
David Terian <dterian64@outlook.com>
2026-01-26 08:28:35 -0800

    added staticwebapp.config.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,4 +12,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
 });
