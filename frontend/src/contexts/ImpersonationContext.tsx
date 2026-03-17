723fd6fe4dee851cc5ebc357c52138a7f3a8cac4
David Terian <dterian64@outlook.com>
2026-03-16 17:41:46 -0700

    empty commit

--- frontend/src/contexts/ImpersonationContext.tsx
+++ frontend/src/contexts/ImpersonationContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect} from 'react';
 import type { ReactNode } from 'react';
 
 import { useMsal } from '@azure/msal-react';
-
+ 
 interface ImpersonationContextType {
   impersonatedUser: ImpersonatedUser | null;
   isImpersonating: boolean;
